bfda80f0e1c9b2b1663cb551b6904f97c61a1e40
JihuaWang007 <86032057+JihuaWang007@users.noreply.github.com>
2021-06-21 17:14:46 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+from __future__ import division
+
+# 即使在python2.X，使用print就得像python3.X那样加括号使用。
+# 导入python未来支持的语言特征division(精确除法)，
+# 当我们没有在程序中导入该特征时，"/"操作符执行的是截断除法(Truncating Division)；
+# 当我们导入精确除法之后，"/"执行的是精确除法, "//"执行截断除除法
+
+# python class时，必有函数__init__:类属性, __call__：调用类的函数（形参）
